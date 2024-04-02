2e94fda815b73e08d2ca1ea151933418f25aecb8
vladyajs <vladshipay@gmail.com>
2024-04-02 21:52:57 +0300

    Test stage

---
+++ solichmag/src/main/java/com/example/solichmag/SolichmagApplication.java
@@ -0,0 +1,14 @@
+package com.example.solichmag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolichmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolichmagApplication.class, args);
+
+	}
+
+}
---
+++ solichmag/src/test/java/com/example/solichmag/SolichmagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solichmag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolichmagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
