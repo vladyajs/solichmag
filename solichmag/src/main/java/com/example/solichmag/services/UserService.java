605d28e86a0b9b0007db0a55cf112b807b328a57
Mantrov17 <115896952+Mantrov17@users.noreply.github.com>
2024-05-25 11:27:38 +0300

    Add files via upload

---
+++ solichmag/src/main/java/com/example/solichmag/services/UserService.java
@@ -0,0 +1,76 @@
+package com.example.solichmag.services;
+
+import com.example.solichmag.models.User;
+import com.example.solichmag.models.enums.Role;
+import com.example.solichmag.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    public boolean createUser(User user){
+        String email = user.getEmail();
+        if (userRepository.findByEmail(user.getEmail()) != null) return false;
+        user.setActive(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("Сохраняем юзера с почтой {}", email);
+        userRepository.save(user);
+        return true;
+    }
+
+
+
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user!=null){
+            if (user.isActive()){
+                user.setActive(false);
+                log.info("Пользователь с id {} и почтой {} был забанен", user.getId(), user.getEmail());
+            } else {
+                user.setActive(true);
+                log.info("Пользователь с id {} и почтой {} был разбанен", user.getId(), user.getEmail());
+            }
+
+
+        }
+
+        userRepository.save(user);
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
+    }
+
+}
